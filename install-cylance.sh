25987b8eff0d163e7e1e0512f21a6f7b716cf916
mhanief <47133144+mhanief@users.noreply.github.com>
2022-04-20 11:34:05 +0800

    Update install-cylance.sh

--- install-cylance.sh
+++ install-cylance.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Install Cylance pre-req packages
 sudo apt-get update -y && sudo apt-get install libxml2-utils make gcc bzip2 linux-headers-generic -y
-# Create config file
-sudo mkdir /opt/cylance/
+# Create Cylance config file
+sudo mkdir /opt/cylance/ -p
 sudo touch /opt/cylance/config_defaults.txt
 sudo echo InstallToken=XXXXXXXXXXXXXXXXXX > /opt/cylance/config_defaults.txt
 sudo echo SelfProtectionLevel=2 >> /opt/cylance/config_defaults.txt
@@ -11,8 +11,12 @@ sudo echo VenueZone=WorkCloud >> /opt/cylance/config_defaults.txt
 sudo echo UiMode=2 >> /opt/cylance/config_defaults.txt
 # Install Cylance
 sudo dpkg -i /tmp/cylance-protect.1404.x86_64.deb
-# start Cylance service
-echo "Checking Cylance Status --> $(service cylancesvc status)"
+# Check Cylance service after installed
+echo "Checking Cylance Status --> $(status cylancesvc)"
+# Manually Start Cylance
+#start cylancesvc
+# Uninstall Cylance
+#dpkg -P cylance-protect
 # Remove Cylance Installer
 #sudo rm -rf /tmp/cylance-protect.1404.x86_64.deb
 # END
